07ce200dbc8e1ab9cd681ad826e98f6f512c6295
Ryan <pepperryan1@gmail.com>
2026-01-22 17:09:09 -0500

    Implementing rightward movement of aliens

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -32,6 +32,7 @@ class AlienInvasion:
             self._check_events()
             self.ship.update()
             self._update_bullets()
+            self._update_aliens()
             self._update_screen()
             self.clock.tick(60)
 
@@ -69,6 +70,10 @@ class AlienInvasion:
         for bullet in self.bullets.copy():
             if bullet.rect.bottom <= 0:
                 self.bullets.remove(bullet)
+
+    def _update_aliens(self):
+        """Update the positions of all aliens in the fleet."""
+        self.aliens.update()
 
     def _update_screen(self):
         """Update images on the screen, and flip to the new screen."""
