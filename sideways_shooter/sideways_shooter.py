ba8aea14c1c86b7e8b06c4eed21f1c1b773f7de5
Ryan <pepperryan1@gmail.com>
2026-02-11 17:36:54 -0500

    Updating sideways_shooter to Alien Invasion progress at a high level

--- sideways_shooter/sideways_shooter.py
+++ sideways_shooter/sideways_shooter.py
@@ -4,6 +4,7 @@ import pygame
 from settings import Settings
 from rocket import Rocket
 from bullet import Bullet
+from alien import Alien
 
 class SidewaysShooter:
     """Overall class to manage game assets and behavior."""
@@ -21,6 +22,9 @@ class SidewaysShooter:
 
         self.rocket = Rocket(self)
         self.bullets = pygame.sprite.Group()
+        self.aliens = pygame.sprite.Group()
+
+        self._create_fleet()
 
     def run_game(self):
         """Start the main loop for the game."""
@@ -28,8 +32,47 @@ class SidewaysShooter:
             self._check_events()
             self.rocket.update()
             self._update_bullets()
+            self._update_aliens()
             self._update_screen()
             self.clock.tick(60)
+
+    def _create_fleet(self):
+        """Create the fleet of aliens."""
+        # Create an alien and keep adding aliens until there's no room left.
+        # Spacing between aliens is one alien width and one alien height.
+        alien = Alien(self)
+        alien_width, alien_height = alien.rect.size
+
+        current_x, current_y = alien_width, alien_height
+        while current_y < (self.settings.screen_height - 3 * alien_height):
+            while current_x < (self.settings.screen_width - 2 * alien_width):
+                self._create_alien(current_x, current_y)
+                current_x += 2 * alien_width
+                
+            # Finished a row; reset x value, and increment y value.
+            current_x = alien_width
+            current_y += 2 * alien_height
+
+    def _create_alien(self, x_position, y_position):
+        """Create an alien and place it in the fleet."""
+        new_alien = Alien(self)
+        new_alien.x = x_position
+        new_alien.rect.x = x_position
+        new_alien.rect.y = y_position
+        self.aliens.add(new_alien)
+
+    def _check_fleet_edges(self):
+        """Respond appropriately if any aliens have reached an edge."""
+        for alien in self.aliens.sprites():
+            if alien.check_edges():
+                self._change_fleet_direction()
+                break
+
+    def _change_fleet_direction(self):
+        """Drop the entire fleet and change the fleet's direction."""
+        for alien in self.aliens.sprites():
+            alien.rect.y += self.settings.fleet_drop_speed
+        self.settings.fleet_direction *= -1
 
     def _update_bullets(self):
         """Update position of bullets and get rid of old bullets."""
@@ -40,6 +83,23 @@ class SidewaysShooter:
         for bullet in self.bullets.copy():
             if bullet.rect.left >= self.screen.get_rect().right:
                 self.bullets.remove(bullet)
+                
+        self._check_bullet_alien_collisions()
+
+    def _check_bullet_alien_collisions(self):
+        """Respond to bullet-alien collisons."""
+        #  Remove any bullets and aliens that have collided.
+        collisions = pygame.sprite.groupcollide(
+            self.bullets, self.aliens, True, True)
+        
+        if not self.aliens:
+            self.bullets.empty()
+            self._create_fleet()
+
+    def _update_aliens(self):
+        """Check if the fleet is at an edge, then update positions."""
+        self._check_fleet_edges()
+        self.aliens.update()
 
     def _update_screen(self):
         """Update images on the screen, and flip to the new screen."""
@@ -47,6 +107,7 @@ class SidewaysShooter:
         for bullet in self.bullets.sprites():
             bullet.draw_bullet()
         self.rocket.blitme()
+        self.aliens.draw(self.screen)
 
         pygame.display.flip()
 
