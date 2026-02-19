a396693f8bf86428925756d71cb8c749f7f1aac0
Ryan <pepperryan1@gmail.com>
2026-02-19 18:05:21 -0500

    Updating sideways_shooter to alien_invasion feature set

--- sideways_shooter/sideways_shooter.py
+++ sideways_shooter/sideways_shooter.py
@@ -43,20 +43,20 @@ class SidewaysShooter:
         alien = Alien(self)
         alien_width, alien_height = alien.rect.size
 
-        current_x, current_y = alien_width, alien_height
-        while current_y < (self.settings.screen_height - 3 * alien_height):
-            while current_x < (self.settings.screen_width - 2 * alien_width):
+        current_x, current_y = self.rocket.rect.right + 2 * alien_width, alien_height
+        while current_x < (self.settings.screen_width -    alien_width):
+            while current_y < (self.settings.screen_height - 2 * alien_height):
                 self._create_alien(current_x, current_y)
-                current_x += 2 * alien_width
+                current_y += 2 * alien_height
                 
             # Finished a row; reset x value, and increment y value.
-            current_x = alien_width
-            current_y += 2 * alien_height
+            current_y = alien_height
+            current_x += 2 * alien_width
 
     def _create_alien(self, x_position, y_position):
         """Create an alien and place it in the fleet."""
         new_alien = Alien(self)
-        new_alien.x = x_position
+        new_alien.y = y_position
         new_alien.rect.x = x_position
         new_alien.rect.y = y_position
         self.aliens.add(new_alien)
